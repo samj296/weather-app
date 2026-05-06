0b4f81420868b8e21c5564fac8e0f96861b83035
Sam J <sam_joseph@live.com>
2026-05-06 15:58:01 -0700

    feat: scaffold initial React weather app structure

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import HomePage from "./pages/HomePage";
+function App(){
+
+  return(
+    <div>
+      <HomePage/>
+    </div>
+  );
+};
+
+
+export default App
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+
+function HomePage(){
+    const [city, setCity] = useState("")
+    return(
+        <div>
+            <h1>Welcome to Weather App</h1>
+            <select>
+                
+            </select>
+        </div>
+    )
+};
+
+export default HomePage;
